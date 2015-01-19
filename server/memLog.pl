1442a868e1d09640044f7f5ca2b00197be3322cc
etheleon <picy2k@gmail.com>
2015-01-19 14:38:58 +0800

    fixed bug in memlog missing pipe, and added sleep argument

--- server/memLog.pl
+++ server/memLog.pl
@@ -5,10 +5,10 @@ use v5.18;
 use autodie;
 # capture the output of system command
 
-die "$0 <processname> <uniqueIdentifier> <anti> insufficient arguments" unless $#ARGV == 2;
-my ($processname, $uniq, $anti) = @ARGV;
+die "$0 <processname> <uniqueIdentifier> <anti> <sleep> insufficient arguments" unless $#ARGV == 3;
+my ($processname, $uniq, $anti, $sleep) = @ARGV;
 
-my $cmd = qq(ps axww -o cmd,size | grep -v "grep"| grep -v "perl");
+my $cmd = qq(ps axww -o cmd,size | grep -v "grep"| grep -v "perl" |);
 if($anti)
 {
     $cmd .= qq(grep -v $uniq | grep $processname);
@@ -23,6 +23,6 @@ while($ps ne '')
 {
     $ps =~ m/\s(?<memusage>\d+)$/;  #in kilobytes
     say eval{ $+{memusage} / 1000000 },"G";
-    sleep 60;
+    sleep $sleep;
     my $ps  = `$cmd`;
 }
